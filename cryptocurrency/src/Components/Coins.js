59567d7f0bc7aa4480185401c00708370d488aba
kundan gupta <kukundan1993@gmail.com>
2023-06-19 23:31:55 +0530

    feature : UpdatedApi crypto

---
+++ cryptocurrency/src/Components/Coins.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Coins({ name, icon, price, symbol }) {
+  return (
+    <>
+    <div className="coin">
+      <h1> Name: {name}</h1>
+      <image src={icon}/>
+      <h3> Price: {price}</h3>
+      <h3> Symbol: {symbol}</h3>
+    </div>
+    </>
+  );
+}
+
+export default Coins;
